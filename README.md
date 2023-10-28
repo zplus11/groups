276599df1a1801373edbcd35844bfbaf74712531
Naman Taggar <namantaggar.11@gmail.com>
2023-10-28 15:44:46 +0530

    final upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # symmetries
 Dihedral groups: symmetries of a regular polygon. Calculate composition of functions in the group easily.
+
+⚠️ To use the program you need to have Python installed. Download it from [here](https://www.python.org/downloads/).
+
+To use it, clone the repository in your desired directory, then after navigating to the directory, run
+```python```
+to start python. Then run
+```from symmetries import *```
+to install the functions. Run
+```help()```
+to learn about the functions. Thankyou for using!
+
--- symmetries.py
+++ symmetries.py
@@ -1,31 +1,49 @@
-sides = 9
-nature = "o"
-if sides%2 == 0:
-    nature = "e"
-polygon = [i + 1 for i in range(sides)]
-original = [i + 1 for i in range(sides)]
-los = []
-if nature == "e":
-    for i in range(sides):
-        #print("r{0}: anti-clockwise rotation of {1} degrees".format(i, i*360/sides))
-        los.append(f"r{i}")
-    for i in range(int(sides/2)):
-        #print("d{0}: reflection about vertex {1}".format(i, i + 1))
-        los.append(f"d{i}")
-    for i in range(int(sides/2)):
-        #print("f{0}: reflection about axis between vertices {1} and {2}".format(i, i + 1, i + 2))
-        los.append(f"f{i}")
-else:
-    for i in range(sides):
-        #print("r{0}: anti-clockwise rotation of {1} degrees".format(i, i*360/sides))
-        los.append(f"r{i}")
-    for i in range(sides):
-        #print("d{0}: reflection about the vertex {1}".format(i, i + 1))
-        los.append(f"d{i}")
+print("Calculate function compositions in dihedral groups easily and quickly!\nRun shelp() for a brief description of all functions.")
 
-def process_symmetry(lok):
-    print(lok)
+nos, nature = 7, "o"
+if nos%2 == 0: nature = "e"
+
+def generate_los(sides):
+    los = []
+    if nature == "e":
+        for i in range(nos):
+            los.append(f"r{i}") # rotation of i*360/n degrees
+        for i in range(int(nos/2)):
+            los.append(f"d{i + 1}") # reflection about vertex number i + 1
+        for i in range(int(nos/2)):
+            los.append(f"f{i + 1}{i + 2}") # reflection about axis between vertices numbered i + 1 & i + 2
+    else:
+        for i in range(nos):
+            los.append(f"r{i}") # rotation of i*360/n degrees
+        for i in range(nos):
+            los.append(f"d{i + 1}") # rotation about vertex number i + 1
+    return los
+
+def los(sides):
+    nature = "o"
+    if sides % 2 == 0: nature = "e"
+    if nature == "e":
+        for i in range(sides):
+            print("r{0}: Anti clockwise rotation of {0} degrees".format(i*360/sides))
+        for i in range(int(sides/2)):
+            print("d{0}: Reflection about vertex number {0}".format(i + 1))
+        for i in range(int(sides/2)):
+            print("f{0}{1}: Reflection about axis between vertices numbered {0} & {1}".format(i + 1, i + 2))
+    else:
+        for i in range(nos):
+            print("r{0}: Anti clockwise rotation of {0} degrees".format(i*360/sides))
+        for i in range(nos):
+            print("d{0}: Reflection about vertex number {0}".format(i + 1))
+            
+def symmetry(lok, sides = nos):
+    los = generate_los(sides)
+    nature = "e"
+    if sides % 2 == 1: nature = "o"
+    polygon = [i + 1 for i in range(sides)]
+    original = polygon.copy()
     keys = [element.strip() for element in lok.split(",")]
+    keys.reverse()
+    print(f"Processing the symmetries in order: {keys}")
     if all(key in los for key in keys):
         for key in keys:
             index = int(key[-1])
@@ -35,10 +53,30 @@ def process_symmetry(lok):
                     if new_vertex == 0:
                         new_vertex = sides
                     polygon[j] = new_vertex
-        print(f"Success.\nOriginal:\t{original}\nUpdated:\t{polygon}")
+            elif key.startswith("f"):
+                for j in range(int(sides/2)):
+                    temp_vertex = polygon[index + j - 1]
+                    polygon[index + j - 1] = polygon[index - j - 2]
+                    polygon[index - j - 2] = temp_vertex
+            elif key.startswith("d"):
+                if nature == "e":
+                    for j in range(int(sides/2) - 1):
+                        temp_vertex = polygon[index + j]
+                        polygon[index + j] = polygon[index - j - 2]
+                        polygon[index - j - 2] = temp_vertex
+                else:
+                    for j in range(int((sides + 1)/2) - 1):
+                        temp_vertex = polygon[index + j]
+                        polygon[index + j] = polygon[index - j - 2]
+                        polygon[index - j - 2] = temp_vertex
+                
+        print(f"Success. Final image:\nOriginal:\t{original}\nUpdated:\t{polygon}")
     else:
         print("At least one of them was not a valid symmetry. Try again. See below the list of valid symmetry for your polygon:")
         print(f"Polygon:\t{polygon}\nSymmetries:\t{los}")
-        print("Feed number of sides to the 'sides' variable to change number of sides.")
 
-process_symmetry("r3,r1,r0")
+def shelp():
+    print("Dihedral Group Symmetries. The following functions are available:\n--- shelp(): This.\nsymmetry('string', n): where 'string' is the list of symmetries you want to apply (separate them with comma) and 'n' is the number of sides in your polygon.\n--- los(n): Generate the list of valid symmetries for a regular polygon of sides 'n'\n--- ssource(): To get the link of github repo.")
+
+def ssource():
+    print("https://github.com/zplus11/symmetries.git")
