219b7965ebaae8b4ef2fa184a27ec3f945a33b7c
Naman Taggar <namantaggar.11@gmail.com>
2023-12-29 20:50:06 +0530

    change stuff

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Dihedral groups: symmetries of a regular polygon. Calculate composition of funct
 
 ⚠️ To use the program you need to have Python installed. Download it from [here](https://www.python.org/downloads/).
 
-To use it, clone the repository in your desired directory, then after navigating to the directory, run
+⚠️ This program is still work in progress. There are bugs and glitches that need to be solved, and there is more to be done overall. It is NOWHERE near its final shape.
+
+To still use it, clone the repository in your desired directory, then after navigating to the directory, run
 
 ```         
 python
@@ -16,10 +18,18 @@ to start python. Then run
 from symmetries import *
 ```
 
-to install the functions. Run
+to install the functions.
 
-```         
-shelp()
+`symmetries` comes with a class `polygon` which can be used to define a regular polygon with `n` sides by running
+
 ```
+pol = polygon(n) # replace n by integer > 2```
 
-to learn about the functions. Thankyou for using!
+and which has the following methods:
+- `show()`: to print the polygon in its current shape;
+- `list()`: to print a list of symmetries available for the polygon, and instructions to use:
+- `apply(operations)`: to apply the operations `operations` to the polynomial.
+
+`apply()` takes a string argument wherein you provide the operations separated by comma.
+
+Thanks for using!
--- symmetries.py
+++ symmetries.py
@@ -1,86 +1,85 @@
-print("Calculate function compositions in dihedral groups easily and quickly!\nRun shelp() for a brief description of all functions.")
+"""python program to calculate Dihedral group symmetries easily"""
 
-nos, nature = 4, "o"
-if nos%2 == 0: nature = "e"
-
-def generate_los(sides):
-    los = []
-    nature = "e"
-    if sides % 2 == 1: nature = "o"
-    if nature == "e":
-        for i in range(sides):
-            los.append(f"r{i}") # rotation of i*360/n degrees
-        for i in range(int(sides/2)):
-            los.append(f"d{i + 1}") # reflection about vertex number i + 1
-        for i in range(int(sides/2)):
-            los.append(f"f{i + 1}{i + 2}") # reflection about axis between vertices numbered i + 1 & i + 2
-    else:
-        for i in range(sides):
-            los.append(f"r{i}") # rotation of i*360/n degrees
-        for i in range(sides):
-            los.append(f"d{i + 1}") # rotation about vertex number i + 1
-    return los
-
-def los(sides):
-    nature = "o"
-    if sides % 2 == 0: nature = "e"
-    if nature == "e":
-        for i in range(sides):
-            print("r{0}: Anti clockwise rotation of {1} degrees".format(i, i*360/sides))
-        for i in range(int(sides/2)):
-            print("d{0}: Reflection about vertex number {0}".format(i + 1))
-        for i in range(int(sides/2)):
-            print("f{0}{1}: Reflection about axis between vertices numbered {0} & {1}".format(i + 1, i + 2))
-    else:
-        for i in range(sides):
-            print("r{0}: Anti clockwise rotation of {1} degrees".format(i, i*360/sides))
-        for i in range(sides):
-            print("d{0}: Reflection about vertex number {0}".format(i + 1))
-            
-def symmetry(lok, sides = nos):
-    los = generate_los(sides)
-    nature = "e"
-    if sides % 2 == 1: nature = "o"
-    polygon = [i + 1 for i in range(sides)]
-    original = polygon.copy()
-    keys = [element.strip() for element in lok.split(",")]
-    keys.reverse()
-    print(f"Processing the symmetries in order: {keys}")
-    print(sides)
-    print(los)
-    if all(key in los for key in keys):
-        for key in keys:
+def f_applysymmetries(keys, polygon):
+    n = len(polygon)
+    ops = [key.lower().strip() for key in keys.split(",")[::-1]]
+    if all(op in f_getlos(n) for op in ops):
+        for key in ops:
             index = int(key[-1])
             if key.startswith("r"):
-                for j in range(sides):
-                    new_vertex = (polygon[j] + index) % sides
+                for j in range(n):
+                    new_vertex = (polygon[j] + index) % n
                     if new_vertex == 0:
-                        new_vertex = sides
+                        new_vertex = n
                     polygon[j] = new_vertex
             elif key.startswith("f"):
-                for j in range(int(sides/2)):
+                for j in range(int(n/2)):
                     temp_vertex = polygon[index + j - 1]
-                    polygon[(index + j - 1) % sides] = polygon[index - j - 2]
-                    polygon[(index - j - 2) % sides] = temp_vertex
+                    polygon[(index + j - 1) % n] = polygon[index - j - 2]
+                    polygon[(index - j - 2) % n] = temp_vertex
             elif key.startswith("d"):
-                if nature == "e":
-                    for j in range(int(sides/2) - 1):
+                if n % 2 == 0:
+                    for j in range(int(n/2) - 1):
                         temp_vertex = polygon[(index + j) % 2]
-                        polygon[(index + j) % sides] = polygon[index - j - 2]
+                        polygon[(index + j) % n] = polygon[index - j - 2]
                         polygon[index - j - 2] = temp_vertex
                 else:
-                    for j in range(int((sides + 1)/2) - 1):
-                        temp_vertex = polygon[(index + j) % sides]
-                        polygon[(index + j) % sides] = polygon[index - j - 2]
+                    for j in range(int((n + 1)/2) - 1):
+                        temp_vertex = polygon[(index + j) % n]
+                        polygon[(index + j) % n] = polygon[index - j - 2]
                         polygon[index - j - 2] = temp_vertex
-                
-        print(f"Success. Final image:\nOriginal:\t{original}\nUpdated:\t{polygon}")
+    return polygon
+
+def f_getlos(n):
+    los = {}
+    if n % 2 == 0:
+        for i in range(n):
+            los[f"r{i}"] = f"Anti clockwise rotation of {i*360/n} degrees" # rotation of i*360/n degrees
+        for i in range(int(n/2)):
+            los[f"d{i + 1}"] = f"Reflection about vertex number {i + 1}" # reflection about vertex number i + 1
+        for i in range(int(n/2)):
+            los[f"f{i + 1}{i + 2}"] = f"Reflection about axis between vertices numbered {i + 1} & {i + 2}" # reflection about axis between vertices numbered i + 1 & i + 2
     else:
-        print("At least one of them was not a valid symmetry. Try again. See below the list of valid symmetry for your polygon:")
-        print(f"Polygon:\t{polygon}\nSymmetries:\t{los}")
+        for i in range(n):
+            los[f"r{i}"] = f"Anti clockwise rotation of {i*360/n} degrees" # rotation of i*360/n degrees
+        for i in range(n):
+            los[f"d{i + 1}"] = f"Reflection about vertex number {i + 1}" # rotation about vertex number i + 1
+    return los
 
-def shelp():
-    print("Dihedral Group Symmetries. The following functions are available:\n--- shelp(): This.\n--- symmetry('string', n): where 'string' is the list of symmetries you want to apply (separate them with comma) and 'n' is the number of sides in your polygon.\n--- los(n): Generate the list of valid symmetries for a regular polygon of sides 'n'\n--- ssource(): To get the link of github repo.")
 
-def ssource():
-    print("https://github.com/zplus11/symmetries.git")
+class polygon:
+    def __init__(self, sides: int):
+        assert sides > 2, "Number of sides must be more than 2 (two)"
+        self.sides = sides
+        self.polygon = {i: i + 1 for i in range(sides)}
+        self.los = f_getlos(self.sides)
+        
+    def list(self):
+        """list available symmetries for the polygon"""
+        
+        print(f"Printing symmetries of regular polygon with {self.sides} sides")
+        for s in self.los:
+            print(s + ":", self.los[s])
+        print("To apply the operations, use 'apply(str)' method where str must be a string of the above symmetries separated by comma. e.g.: square.apply('r0, r3, d1')")
+        
+    def show(self):
+        """show the polygon in its current shape"""
+        
+        print("The polygon in current shape is:")
+        print([i for i in self.polygon.values()])
+
+    def apply(self, operations: str = "r0"):
+        """apply symmetries to the polygon. separate the operations in list() method by commas"""
+        
+        o_polygon = [i for i in self.polygon.copy().values()]
+        self.polygon = f_applysymmetries(operations, self.polygon)
+        print("Operations applied. The polygon was changed from")
+        print(o_polygon)
+        print("to")
+        print([i for i in self.polygon.values()])
+
+
+triangle = polygon(3)
+square = polygon(4)
+pentagon = polygon(5)
+hexagon = polygon(6)
