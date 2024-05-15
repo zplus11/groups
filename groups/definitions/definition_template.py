449429b198cd93b4b26a3041d6ea30afcb534808
Naman Taggar <namantaggar.11@gmail.com>
2024-05-15 19:46:39 +0530

    Small fix

--- groups/definitions/definition_template.py
+++ groups/definitions/definition_template.py
@@ -1,18 +1,18 @@
 # groups\definitions\definition_template.py
 
-# In groups, groups are defined in their own classes
-# that derive from the group class that is parent to all.
-# To define a group yourself here, you need to define the
+# In this library, groups are defined in their own classes
+# that derive from the parent-to-all `group` class. To
+# define a group yourself here, you need to define the
 # following aspects:
 #   - its identity,
 #   - its members, and
-#   - how operations are defined.
-# The former two are stored in the respective class
-# attributes, namely self.identity and self.members. For
+#   - how operations are applied.
+# The former two are stored in their respective class
+# attributes namely self.identity and self.members. For
 # the third, you need to define a method that takes group
 # members as input and returns the 'image' when these
 # members are operated on self.identity. Below is a basic
-# structure of how the above may be done.
+# structure of how it may be done.
 
 
 # Import the parent group from core
@@ -27,7 +27,7 @@ class mygroup(group):
         super().__init__(identity, members)
 
     def apply(self, *elements):
-        return sum(elements) % 2
+        return sum(elements) % 3
 
 
 # Then, this group can be freely called and you can use
@@ -37,13 +37,13 @@ class mygroup(group):
 # >>> mg = mygroup()
 # >>> mg.members
 # [0, 1, 2]
-# >>> mg.apply(0, 1, 2, 2, 1)
+# >>> mg.apply(0, 1, 2, 1, 1)
 # 0
 # >>> mg.subgroups()
 # [{0}, {0, 1, 2}]
 # >>> mg.nsubgroups()
 # [{0}, {0, 1, 2}]
 # >>> mg.coset({0, 1, 2}, 2)
-# {0, 1}
+# {0, 1, 2}
 
 # End of definition_template.py
